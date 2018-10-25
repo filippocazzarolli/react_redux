251c23d0f65f421b0ee995773b68ee24a221606f
unknown <filippocazzarolli@gmail.com>
2018-10-25 21:57:33 +0200

    redux react devtools

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,12 +1,23 @@
-import {createStore, applyMiddleware} from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import rootReducers from '../reducers'
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant'
 import thunk from 'redux-thunk'
 
+// export default function configureStore(initialState) {
+//     return createStore(
+//         rootReducers,
+//         initialState,
+//         applyMiddleware(thunk, reduxImmutableStateInvariant())
+//     )
+// }
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export default function configureStore(initialState) {
     return createStore(
-        rootReducers,
-        initialState,
-        applyMiddleware(thunk, reduxImmutableStateInvariant())
+        rootReducers, 
+        initialState, 
+        composeEnhancers(
+            applyMiddleware(thunk, reduxImmutableStateInvariant())
+        )
     )
 }
